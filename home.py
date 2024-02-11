671dff1c7a1825db8ef463f4a963982498166862
BEUMGOO KIM <captain_kbg@naver.com>
2024-02-12 00:59:21 +0900

    document base

---
+++ home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="🤖",
+)
+
+st.markdown(
+    """          
+## KBGPTへようこそ！ 
+
+### 以下のアプリをお試しください。
+            
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
